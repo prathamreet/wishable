18a0b9d6221e322811afca98b7cda649cd15f751
Pratham Reet <prathamreet@gmail.com>
2025-05-03 22:00:48 +0530

    prod fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,25 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+      },
+      colors: {
+        amber: {
+          50: '#fffbeb',
+          100: '#fef3c7',
+          200: '#fde68a',
+          300: '#fcd34d',
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309',
+          800: '#92400e',
+          900: '#78350f',
+          950: '#451a03',
+        },
+      },
     },
   },
   plugins: [],
-} 
+}
